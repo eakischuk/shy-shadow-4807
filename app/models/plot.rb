8bce27b34662991a40c4ed506f72fa0ea972939d
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-22 10:36:10 -0600

    Add instance method for plot plant for

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -2,4 +2,8 @@ class Plot < ApplicationRecord
   belongs_to :garden
   has_many :plot_plants, dependent: :destroy
   has_many :plants, through: :plot_plants
+
+  def plot_plant_for(plant)
+    PlotPlant.specific_plot_plant(id, plant)
+  end
 end
