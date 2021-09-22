2e8293788da625e9ebf9f119397be886f137c5d7
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-22 10:27:22 -0600

    Add specific plot_plant class method and destroy dependencies

--- app/models/plot_plant.rb
+++ app/models/plot_plant.rb
@@ -1,4 +1,8 @@
 class PlotPlant < ApplicationRecord
   belongs_to :plot
   belongs_to :plant
+
+  def self.specific_plot_plant(plot, plant)
+    where('plot_id = ? AND plant_id = ?', plot, plant).last
+  end
 end
